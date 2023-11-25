12bff705654969f22c18c25d1bd01ae42edc44fd
Tishtanya Shaw <tish@tishtanyas-air.mynetworksettings.com>
2023-11-25 18:15:44 -0500

    Updated settings

--- django_beauty/settings.py
+++ django_beauty/settings.py
@@ -183,13 +183,13 @@ REST_FRAMEWORK = {
 }
 
 #
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# AWS_STORAGE_BUCKET_NAME = 'keita-beauty-store'
+AWS_STORAGE_BUCKET_NAME = 'keita-beauty-store'
 
-# AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = 'public-read'
 
-# AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-# AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
-# AWS_QUERYSTRING_AUTH = False
+AWS_QUERYSTRING_AUTH = False
