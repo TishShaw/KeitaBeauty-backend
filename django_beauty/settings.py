2889f01d0f2adb95526b2800f13ba3ac1996bae3
Tishtanya Shaw <tish@tishtanyas-air.mynetworksettings.com>
2023-11-25 22:15:04 -0500

    Make changes to settings.py

--- django_beauty/settings.py
+++ django_beauty/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from storages.backends.s3boto3 import S3Boto3Storage
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
